bfd53d3379ea4b8b7211287211874722bde861fc
Jakub Filistyński <filistynskikuba@gmail.com>
2021-05-02 23:55:24 +0200

    dodanie factory boy oraz factory do użytkowników

---
+++ apps/users/factories.py
@@ -0,0 +1,17 @@
+import factory
+
+from apps.users.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker("company_email")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    email = username
+    is_staff = False
+    is_superuser = False
+    is_active = True
+    password = factory.Faker("password")
