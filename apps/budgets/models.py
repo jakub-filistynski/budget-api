30a9ec00534cb3c20d40d7dbefdd28eb5a133eea
Jakub Filistyński <filistynskikuba@gmail.com>
2021-05-03 01:16:17 +0200

    model budżetu + migracja

--- apps/budgets/models.py
+++ apps/budgets/models.py
@@ -1,3 +1,18 @@
-# from django.db import models
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from apps.users.models import User
+from apps.utils.models import BaseModel
+
+
+class Budget(BaseModel):
+    name = models.CharField(max_length=100, verbose_name=_("Nazwa budżetu"))
+    owner = models.ForeignKey(User, related_name="budgets", on_delete=models.CASCADE)
+    members = models.ManyToManyField(
+        User, through="Membership", related_name="shared_budgets"
+    )
+
+
+class Membership(models.Model):
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
