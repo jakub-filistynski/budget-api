3104d58416972f70a51c75ab33e1914a380516a3
Jakub Filistyński <filistynskikuba@gmail.com>
2021-05-03 15:52:43 +0200

    przeniesienie listy budżetów do budgets

--- apps/users/api/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import IsAuthenticated
-
-
-class IsAccountOwnerPermission(IsAuthenticated):
-    def has_permission(self, request, view):
-        result = super().has_permission(request, view)
-        return result and request.user.pk == view.kwargs.get("pk")
--- apps/users/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from apps.users.api.views import UserBudgetsListView
-
-urlpatterns = [
-    path("<uuid:pk>/budgets/", UserBudgetsListView.as_view(), name="user_budgets")
-]
