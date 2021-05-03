80dfcad75c5ac8ea114164f2c3d3bd591df36db4
Jakub Filistyński <filistynskikuba@gmail.com>
2021-05-03 18:22:10 +0200

    model finansów

--- apps/finances/models.py
+++ apps/finances/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from apps.budgets.models import Budget
+from apps.categories.models import Category
+from apps.utils.models import BaseModel
+
+
+class Finance(BaseModel):
+
+    name = models.CharField(max_length=100, verbose_name=_("Nazwa kategorii"))
+    money_amount = models.PositiveIntegerField()
+    budget = models.ForeignKey(
+        Budget, related_name="finances", on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        Category, related_name="finances", on_delete=models.CASCADE
+    )
