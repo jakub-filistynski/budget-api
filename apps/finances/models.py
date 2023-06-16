7af0d04c8a9ce6755297ac7ad45ec91351c992fd
Jakub Filistyński <j.filistynski@gmail.com>
2023-06-16 17:11:28 +0200

    Add verbouse name in Finance model

--- apps/finances/models.py
+++ apps/finances/models.py
@@ -8,10 +8,16 @@ from apps.utils.models import BaseModel
 
 class Finance(BaseModel):
     name = models.CharField(max_length=100, verbose_name=_("Nazwa wpłaty/wypłaty"))
-    money_amount = models.PositiveIntegerField()
+    money_amount = models.PositiveIntegerField(verbose_name=_("Kwota"))
     budget = models.ForeignKey(
-        Budget, related_name="finances", on_delete=models.CASCADE
+        Budget,
+        related_name="finances",
+        on_delete=models.CASCADE,
+        verbose_name=_("Budżet"),
     )
     category = models.ForeignKey(
-        Category, related_name="finances", on_delete=models.CASCADE
+        Category,
+        related_name="finances",
+        on_delete=models.CASCADE,
+        verbose_name=_("Kategoria"),
     )
