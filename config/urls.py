b019e8afaef3aebe3b364afe88397acdbfd0c8a1
Jakub Filistyński <j.filistynski@gmail.com>
2023-06-20 15:38:25 +0200

    Add swagger-ui

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+
+from rest_framework.schemas import get_schema_view
 
 api_patterns = [
     path("", include("dj_rest_auth.urls")),
@@ -10,4 +13,22 @@ api_patterns = [
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(api_patterns)),
+    path(
+        "",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="swagger-ui",
+    ),
+    path(
+        "openapi",
+        get_schema_view(
+            title="Budget API",
+            description="API schema",
+            version="1.0.0",
+            permission_classes=[],
+        ),
+        name="openapi-schema",
+    ),
 ]
